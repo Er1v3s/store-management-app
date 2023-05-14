4992bb69abb1a677e4ad57133a2f1446088cd37a
Filip Statkiewicz <stf6.dev@gmail.com>
2023-05-14 16:57:51 +0200

    feat: window manipulation dragging functionality to login view

--- StoreManagementApp/pages/Login.xaml.cs
+++ StoreManagementApp/pages/Login.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace StoreManagementApp.pages
 {
@@ -22,6 +13,14 @@ namespace StoreManagementApp.pages
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
     }
 }
